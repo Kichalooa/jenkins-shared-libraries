714f14032cc0cf399a10bbe19a65af7b1beed845
Kichalooa <shivgupta7780@gmail.com>
2026-07-25 04:52:46 +0530

    Change shell commands to batch commands for Windows

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,7 +1,7 @@
 def call(String Project,String ImageTag, String dockerhubuser)
 {
    withCredentials([usernamePassword('credentialsId': "dockerHubCred",passwordVariable: "dockerHubPass",usernameVariable: "dockerHubUser")])
-   sh "docker login -u ${dockerhubuser} -p ${dockerHubPass}"
-   sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
+   bat "docker login -u ${dockerhubuser} -p ${dockerHubPass}"
+   bat "docker push ${dockerhubuser}/${Project}:${ImageTag}"
   
 }
