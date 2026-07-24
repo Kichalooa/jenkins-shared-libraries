ecf80e134c0d799e087bad79312639b3a7a10542
Kichalooa <shivgupta7780@gmail.com>
2026-07-25 04:55:43 +0530

    Add missing closing brace in docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,7 +1,8 @@
 def call(String Project,String ImageTag, String dockerhubuser)
 {
    withCredentials([usernamePassword('credentialsId': "dockerHubCred",passwordVariable: "dockerHubPass",usernameVariable: "dockerHubUser")])
+   {
    bat "docker login -u ${dockerhubuser} -p ${dockerHubPass}"
    bat "docker push ${dockerhubuser}/${Project}:${ImageTag}"
-  
+   }
 }
